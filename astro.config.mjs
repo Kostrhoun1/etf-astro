ce72d02297c6be805499745dcdd664504595bfc2
Tomas Kostrhoun <tomaskostrhoun@Tomas--MacBook-Air-2.local>
2025-12-30 22:17:38 +0100

    Use hybrid mode for static pages + API routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,16 @@ import { fileURLToPath } from 'url';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.etfpruvodce.cz',
-  output: 'static',
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [react(), sitemap()],
 
   vite: {
